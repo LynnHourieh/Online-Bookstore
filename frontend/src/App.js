3311832c5975020429e3d5cebe7784aad6375714
lynnhourieh <lynnhourieh30@gmail.com>
2023-06-22 17:40:48 +0300

    adding Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import HomeScreen from "./screens/HomeScreen";
 import { Route, Routes } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
 
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
-  
   return (
-    <Fragment>
-      <header>
-        <Link to="/">Online-BookStore</Link>
-      </header>
+    <div className="d-flex flex-column site-container"> 
+      <Navbar bg="light" variant="light">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>BookStore</Navbar.Brand>
+          </LinkContainer>
+        </Container>
+      </Navbar>
 
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/product/:title" element={<ProductScreen />} />
-      </Routes>
-    </Fragment>
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:title" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      
+    </div>
   );
 }
 
