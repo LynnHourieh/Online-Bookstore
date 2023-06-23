d0fffa5f8b46cf5db715ed22e87492178fda9297
lynnhourieh <lynnhourieh30@gmail.com>
2023-06-23 16:33:30 +0300

    add productRoute

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import Product from "../Models/productModel.js";
+
+
+
+const productRouter = express.Router();
+productRouter.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
+});
+productRouter.get("/title/:title", async (req, res) => {
+  const product = await Product.findOne({ title: req.params.title });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+productRouter.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+export default productRouter;
