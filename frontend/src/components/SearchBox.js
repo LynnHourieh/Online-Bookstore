e543e99909ddc6d1e666ae13f856636ec6301394
lynnhourieh <lynnhourieh30@gmail.com>
2025-01-13 18:13:06 +0200

    bookstore-1 update css

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -15,7 +15,7 @@ export default function SearchBox() {
   };
 
   return (
-    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+    <Form className="d-flex search-form" onSubmit={submitHandler}>
       <InputGroup>
         <FormControl
           type="text"
